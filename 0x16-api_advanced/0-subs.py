5bf29214f0befe1d173b2bba387c9c9f5f25542b
Nesgomez9 <nicolico99@hotmail.com>
2020-05-08 02:17:35 +0000

    [Add] Adding task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Function that queries the Reddit API
+and returns the number of subscribers """
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    try:
+        header = {'Nicolas-Gomez': 'My-computador/0.0,1'}
+        url ="https://www.reddit.com/r/{}/about.json".format(subreddit)
+        js = requests.get(url, headers=header, allow_redirects=False)
+        subs = js.json().get('data').get('subscribers')
+    except:
+        return 0
