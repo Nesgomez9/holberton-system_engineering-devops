4c2890f59f080d9ffa7fe7e53981a4bbfb88b74d
Nesgomez9 <nicolico99@hotmail.com>
2020-05-08 02:20:44 +0000

    [Feature] Fixing User-Agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,5 +11,6 @@ def number_of_subscribers(subreddit):
         url ="https://www.reddit.com/r/{}/about.json".format(subreddit)
         js = requests.get(url, headers=header, allow_redirects=False)
         subs = js.json().get('data').get('subscribers')
+        return subs
     except:
         return 0
